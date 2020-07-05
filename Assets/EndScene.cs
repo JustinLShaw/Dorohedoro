edc76ff7592e59689494d01e28e918bc9424fc88
maknee <henryzhu@seas.upenn.edu>
2020-07-04 22:51:41 -0400

    Playing end video

--- Assets/EndScene.cs
+++ Assets/EndScene.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class EndScene : MonoBehaviour
 {
+    public GameObject video_player;
+    VideoPlayer videoPlane;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        videoPlane = video_player.GetComponent<VideoPlayer>();
+        videoPlane.Prepare();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("WinScene");
+        videoPlane.Play();
+        //Destroy(this.gameObject);
     }
 }
